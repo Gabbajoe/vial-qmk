3507b6043c8de86df9a0aec537c020ecc2e95cd6
Gabbajoe <5196156+Gabbajoe@users.noreply.github.com>
2024-05-24 23:54:44 +0200

    added more keymaps for different controlers

---
+++ keyboards/splitkb/aurora/lily58/keymaps/vial-norgb/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBA, 0xBD, 0xC3, 0x25, 0x22, 0x67, 0x9D, 0x88}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
---
+++ keyboards/splitkb/aurora/lily58/keymaps/vial-norgboled/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBA, 0xBD, 0xC3, 0x25, 0x22, 0x67, 0x9D, 0x88}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+// #define VIAL_TAP_DANCE_ENTRIES 16
+// #define VIAL_COMBO_ENTRIES 16
+// #define VIAL_KEY_OVERRIDE_ENTRIES 16
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
+
+
